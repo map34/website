2ed132025931e8305f12f172c3cd250be8babd6a
Adrian Prananda <prananda0203@gmail.com>
2018-07-25 23:28:23 -0700

    resize main profpic + Revert React upgrade because of dev-prod mismatch

--- gatsby-config.js
+++ gatsby-config.js
@@ -55,8 +55,7 @@ module.exports = {
     'gatsby-transformer-sharp',
     'gatsby-plugin-sharp',
     'gatsby-plugin-react-helmet',
-    'gatsby-plugin-sass',
-    'gatsby-plugin-react-next'
+    'gatsby-plugin-sass'
   ],
 };
 
