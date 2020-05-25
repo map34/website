ca33f8758dc2c7b4ea2daf5c3774084be1c3b272
Adrian Prananda <prananda0203@gmail.com>
2020-05-24 18:54:00 -0700

    Fix release

--- release.sh
+++ release.sh
@@ -2,5 +2,7 @@
 
 set -e
 git remote set-url origin https://${GITHUB_TOKEN}@github.com/map34/website.git
+npm install
+git reset --hard && git clean -fd
 npm version patch -m "Upgrade to version %s"
 npm run deploy
