664958ee7926e943df7885e560293c595c86526a
Adrian Prananda <prananda0203@gmail.com>
2020-05-24 19:16:56 -0700

    Test release

--- release.sh
+++ release.sh
@@ -3,6 +3,11 @@
 set -e
 git remote set-url origin https://${GITHUB_TOKEN}@github.com/map34/website.git
 npm install
+
+echo "Deploying GH pages..."
+npm run deploy
+
+echo "Upgrading version..."
 git reset --hard && git clean -fd
 npm version patch -m "Upgrade to version %s"
-npm run deploy
+git push --follow-tags
